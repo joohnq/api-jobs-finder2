d815961dc33861ac8e94f8bb4b35ee4fc7abf1ee
João Henrique <joaohenriquess3287@gmail.com>
2024-01-28 11:49:25 -0300

    fix

--- pages/api/job.ts
+++ pages/api/job.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { id } = req.query;
+    const id = req.query.id as string;
   
     const jobs = allJobs;
   
