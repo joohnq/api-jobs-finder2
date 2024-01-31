d6eb1de23d6d3af707329eb338940502502e7e2f
João Henrique <joaohenriquess3287@gmail.com>
2024-01-31 12:32:25 -0300

    fix

--- pages/api/jobs/popular.ts
+++ pages/api/jobs/popular.ts
@@ -2,11 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import allJobs from "../jobs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const jobs = allJobs;
-    const job = jobs.filter((job) => job.category === "popular");
-    if (!job) {
-      return res.status(404).json({ error: 'Não há nenhum vaga popular' });
-    }
-  
-    res.status(200).json(job);
+  const limit = parseInt(req.query.limit as string, 10) || 10;
+  const jobs = allJobs;
+  const job = jobs.filter((job) => job.category === "popular").slice(0, limit);
+  if (!job) {
+    return res.status(404).json({ error: "Não há nenhum vaga popular" });
   }
+
+  res.status(200).json(job);
+}
--- pages/api/jobs/recent-post.ts
+++ pages/api/jobs/recent-post.ts
@@ -2,11 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import allJobs from "../jobs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const jobs = allJobs;
-    const job = jobs.filter((job) => job.category === "recent-posted");
-    if (!job) {
-      return res.status(404).json({ error: 'Não há nenhum vaga recentemente postada' });
-    }
-  
-    res.status(200).json(job);
+  const limit = parseInt(req.query.limit as string, 10) || 10;
+  const jobs = allJobs;
+  const job = jobs
+    .filter((job) => job.category === "recent-posted")
+    .slice(0, limit);
+  if (!job) {
+    return res
+      .status(404)
+      .json({ error: "Não há nenhum vaga recentemente postada" });
   }
+
+  res.status(200).json(job);
+}
